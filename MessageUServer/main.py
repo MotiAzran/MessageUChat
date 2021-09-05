a69a96ee58223dfd2cdd0e54b1ebe892197cf85b
Moti Azran <motiazran9@gmail.com>
2021-09-06 01:11:41 +0300

    Add server main shield

---
+++ MessageUServer/main.py
@@ -0,0 +1,11 @@
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f'Error: {e}')
+    except:
+        print('Unknown error')
